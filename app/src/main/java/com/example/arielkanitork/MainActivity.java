de5f1d02eab3f93f0e5c952b2d7e3fb55cd217b5
JustCoding124 <99827982+JustCoding124@users.noreply.github.com>
2024-01-27 18:41:47 +0200

    commit 1

--- app/src/main/java/com/example/arielkanitork/MainActivity.java
+++ app/src/main/java/com/example/arielkanitork/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        x=0;
         setContentView(R.layout.activity_main);
     }
 }
